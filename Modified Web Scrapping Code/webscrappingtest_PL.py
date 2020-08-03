84f043f4bc4f92fe4dd3d45bdc073b6ae34d9097
sdpl0118 <47848664+sdpl0118@users.noreply.github.com>
2020-08-03 11:22:29 -0700

    Add files via upload
    Modified Web scraping script, built on original of CaioBrighenti

---
+++ Modified Web Scrapping Code/webscrappingtest_PL.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 24 10:42:19 2020
+
+@author: CaioBrighenti
+@modified: Peter Law
+"""
+
+import os, time
+import selenium
+from selenium import webdriver
+browser = webdriver.Firefox(executable_path=r'D:/bteststat/geckodriver.exe')
+#os.getcwd()
+#os.chdir('C:/Users/Caio/repos/nba-models')
+
+# start browser crawler
+#browser = webdriver.Firefox()
+
+# grab season player stat per game
+scrapeBbalRef(1980, 2020, 'https://www.basketball-reference.com/leagues/NBA_*SEASON*_per_game.html','per_game_stats','D:/bteststat/season-stats-pergame')
+# grab season player stat totals
+scrapeBbalRef(2018, 2018, 'https://www.basketball-reference.com/leagues/NBA_*SEASON*_totals.html','totals_stats','season-stats-totals')
+# grab season advanced player stats
+scrapeBbalRef(1980, 2020, 'https://www.basketball-reference.com/leagues/NBA_*SEASON*_advanced.html','advanced_stats','D:/bteststat/season-stats-advanced')
+# grab MVP stats
+scrapeBbalRef(1980, 2019, 'https://www.basketball-reference.com/awards/awards_*SEASON*.html','nba_mvp','D:/bteststat/mvp-award-stats', False)
+# grab ROY stats (bad)
+scrapeBbalRef(2019, 2019, 'https://www.basketball-reference.com/awards/awards_*SEASON*.html','all_roy','D:/bteststat/roy-award-stats', False)
+# grab DPOY stats (bad)
+scrapeBbalRef(1980, 2019, 'https://www.basketball-reference.com/awards/awards_*SEASON*.html','Defensive_Player_of_the_Year','D:/bteststat/DPOY-award-stats', False)
+# grab 6MOY stats (bad)
+scrapeBbalRef(1980, 2019, 'https://www.basketball-reference.com/awards/awards_*SEASON*.html','Sixth_Man_of_the_Year','D:/bteststat/6MOY-award-stats', False)
+# grab standings stats
+scrapeBbalRef(1980, 2020, 'https://www.basketball-reference.com/leagues/NBA_*SEASON*_standings.html', 'expanded_standings','D:/bteststat/season-standings', False)
+# All star stats (bad)
+scrapeBbalRef(1985, 2018, 'https://www.basketball-reference.com/allstar/NBA_*SEASON*.html', 'East','D:/bteststat/MVP-Roster', False)
+# grab season player stat per game
+scrapeBbalRef(1980, 1980, 'https://www.basketball-reference.com/leagues/NBA_*SEASON*_rookies.html','rookies','D:/bteststat/Rookie-Season-Stats', False)
+
+def scrapeBbalRef(year_start, year_end, page_string, id, folder_name, toggle_partial = True ):
+    for season in range(year_start, year_end+1):
+        #print(season)
+        # navigate to bballref
+        url = page_string.replace("*SEASON*", str(season))
+        browser.get(url)
+
+        # setup id strings
+        partial_id = id + "_toggle_partial_table"
+        csv_id = "csv_" + id
+
+        # activate and grab data in CSV format
+        if toggle_partial:
+            browser.execute_script('document.getElementById("'+ partial_id +'").click();')
+
+        # grab raw CSV
+        raw_csv = browser.execute_script('''var x = document.getElementsByClassName("tooltip");
+        x[3].click();
+        var content = document.getElementById("'''+ csv_id +'''")
+        // small hack to account for a few old MVP pages
+        if (content==null) {
+            var content = document.getElementById("csv_mvp")
+        }
+        return content.textContent
+        ''')
+
+        # clean csv string
+        ## get rid of false headers
+        if page_string == 'https://www.basketball-reference.com/awards/awards_*SEASON*.html' or page_string == 'https://www.basketball-reference.com/leagues/NBA_*SEASON*_standings.html':
+            raw_csv = '\n'.join(raw_csv.split('\n')[2:])
+        ## remove special characters from standings
+        raw_csv=raw_csv.replace("\u2264","")
+        raw_csv=raw_csv.replace("\u2265","")
+
+        #print(raw_csv)
+        # write to CSV
+        pathstr = folder_name + "/" + str(season) + ".csv"
+        f = open(pathstr, "w", encoding="utf-8")
+        f.write(raw_csv)
+
+
+### DEPRECATED, BUILT INTO scrapeBbalRef
+## GRAB SEASON PLAYER STAT PER GAME
+# for season in range(1976, 1977):
+#     # navigate to bballref
+#     url = 'https://www.basketball-reference.com/leagues/NBA_' + str(season) + '_per_game.html'
+#     browser.get(url)
+#     # activate and grab data in CSV format
+#     raw_csv = browser.execute_script('''document.getElementById("per_game_stats_toggle_partial_table").click();
+#     var x = document.getElementsByClassName("tooltip");
+#     x[3].click();
+#     var content = document.getElementById("csv_per_game_stats")
+#     return content.textContent
+#     ''')
+#
+#     # write to CSV
+#     pathstr = "season-stats-pergame/" + str(season) + ".csv"
+#     f = open(pathstr, "w")
+#     f.write(raw_csv)
+# ## GRAB SEASON PLAYER STAT TOTALS
+# for season in range(1976, 2018):
+#     # navigate to bballref
+#     url = 'https://www.basketball-reference.com/leagues/NBA_' + str(season) + '_totals.html'
+#     browser.get(url)
+#     # activate and grab data in CSV format
+#     raw_csv = browser.execute_script('''document.getElementById("totals_stats_toggle_partial_table").click();
+#     var x = document.getElementsByClassName("tooltip");
+#     x[3].click();
+#     var content = document.getElementById("csv_totals_stats")
+#     return content.textContent
+#     ''')
+#
+#     # write to CSV
+#     pathstr = "season-stats-totals/" + str(season) + ".csv"
+#     f = open(pathstr, "w")
+#     f.write(raw_csv)
+# ## GRAB SEASON MVP STATS
+# for season in range(1976, 1977):
+#     # navigate to bballref
+#     url = 'https://www.basketball-reference.com/awards/awards_' + str(season) + '.html'
+#     browser.get(url)
+#     # activate and grab data in CSV format
+#     raw_csv = browser.execute_script('''var x = document.getElementsByClassName("tooltip");
+#     x[3].click();
+#     var content = document.getElementById("csv_mvp")
+#     if (content==null) {
+#         var content = document.getElementById("csv_nba_mvp")
+#     }
+#     return content.textContent
+#     ''')
+#
+#     # write to CSV
+#     raw_csv = raw_csv[133:]
+#     pathstr = "award-stats/" + str(season) + ".csv"
+#     f = open(pathstr, "w")
+#     f.write(raw_csv)
